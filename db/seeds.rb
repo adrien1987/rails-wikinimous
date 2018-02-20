03292ef51cf28701a8b1b87bedd41a1647c874f9
adrien vidal <andras.adrien@gmail.com>
2018-02-20 17:46:39 +0100

    functions ok without css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+10.times do
+  article = Article.create!(title: Faker::HarryPotter.character, content:
+Faker::HarryPotter.book)
+p article
+end
+puts "10 seeds created"
